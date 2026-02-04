d637cbddcaf111c62a2ae7f885c542c626ed2836
CALVIN\mrfai <calvinng7575@gmail.com>
2026-02-04 23:14:59 +0800

    Level1-3: Mark and unmark tasks

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -13,6 +13,7 @@ public class Timer {
         Scanner scanner = new Scanner(System.in);
 
         String[] tasks = new String[MAX_TASKS];
+        boolean[] isDone = new boolean[MAX_TASKS];
         int taskCount = 0;
 
         while (true) {
@@ -27,15 +28,49 @@ public class Timer {
 
             if (input.equals("list")) {
                 printLine();
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + formatTask(tasks[i], isDone[i]));
                 }
                 printLine();
                 continue;
             }
 
+            if (input.startsWith("mark ")) {
+                int index = parseIndex(input, "mark ");
+                if (isValidIndex(index, taskCount)) {
+                    isDone[index] = true;
+                    printLine();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + formatTask(tasks[index], true));
+                    printLine();
+                } else {
+                    printLine();
+                    System.out.println("Please give a valid task number to mark.");
+                    printLine();
+                }
+                continue;
+            }
+
+            if (input.startsWith("unmark ")) {
+                int index = parseIndex(input, "unmark ");
+                if (isValidIndex(index, taskCount)) {
+                    isDone[index] = false;
+                    printLine();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + formatTask(tasks[index], false));
+                    printLine();
+                } else {
+                    printLine();
+                    System.out.println("Please give a valid task number to unmark.");
+                    printLine();
+                }
+                continue;
+            }
+
             if (taskCount < MAX_TASKS) {
                 tasks[taskCount] = input;
+                isDone[taskCount] = false;
                 taskCount++;
 
                 printLine();
@@ -51,5 +86,22 @@ public class Timer {
 
     private static void printLine() {
         System.out.println(LINE);
+    }
+
+    private static String formatTask(String description, boolean done) {
+        return "[" + (done ? "X" : " ") + "] " + description;
+    }
+
+    private static int parseIndex(String input, String prefix) {
+        try {
+            String numberPart = input.substring(prefix.length()).trim();
+            return Integer.parseInt(numberPart) - 1; // convert to 0-based index
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private static boolean isValidIndex(int index, int taskCount) {
+        return index >= 0 && index < taskCount;
     }
 }
