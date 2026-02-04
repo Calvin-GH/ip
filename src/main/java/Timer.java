a9a2ff278bf1cc371d971de1e41b296499f31c76
CALVIN\mrfai <calvinng7575@gmail.com>
2026-02-04 23:12:11 +0800

    Level1-2: Add and list tasks

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Timer {
+    private static final int MAX_TASKS = 100;
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         printLine();
         System.out.println("Hello! I'm Timer");
@@ -8,6 +11,9 @@ public class Timer {
         printLine();
 
         Scanner scanner = new Scanner(System.in);
+
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
 
         while (true) {
             String input = scanner.nextLine();
@@ -19,13 +25,31 @@ public class Timer {
                 break;
             }
 
-            printLine();
-            System.out.println(input);
-            printLine();
+            if (input.equals("list")) {
+                printLine();
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                printLine();
+                continue;
+            }
+
+            if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = input;
+                taskCount++;
+
+                printLine();
+                System.out.println("added: " + input);
+                printLine();
+            } else {
+                printLine();
+                System.out.println("Sorry, I can't add more tasks (limit reached).");
+                printLine();
+            }
         }
     }
 
     private static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
     }
 }
