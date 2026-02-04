04c951d6be518939f833f1d254d9cb819167552d
CALVIN\mrfai <calvinng7575@gmail.com>
2026-02-04 23:04:21 +0800

    Level 1-1: Echo and exit on bye

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Timer {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        printLine();
         System.out.println("Hello! I'm Timer");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                printLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            }
+
+            printLine();
+            System.out.println(input);
+            printLine();
+        }
+    }
+
+    private static void printLine() {
         System.out.println("____________________________________________________________");
     }
 }
